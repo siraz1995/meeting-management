76b533e8f8a7d6c204e3989f0036271a1f87138d
Sirazul Islam <sirazulislam.bd.2014@gmail.com>
2023-10-20 00:40:51 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Button from 'react-bootstrap/Button';
+// import { Stack } from 'react-bootstrap';
+// import { Home } from './components/Home';
+// import { Department } from './components/Department';
+// import {Employee} from './components/Employee';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+ import { Home } from './components/Home';
+// import { Department } from './components/Department';
+// import { Employee } from './components/Employee';
+// import {Navigation} from './components/Navigation';
+//import Sidenav from './Sidenav';
+//import aboutus from './components/aboutus'
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    
+//     <BrowserRouter>
+//     <div className="container">
+//      {/* <h1>Hello World!</h1>
+//      <Stack direction="horizontal" gap={2}>
+//   <Button as="a" variant="primary">
+//     Button as link
+//   </Button>
+//   <Button as="a" variant="success">
+//     Button as link
+//   </Button>
+// </Stack> */}
+// {/* <Home/> */}
+// {/* <Department/> */}
+//  {/* <Employee/> */}
+//  {/* <Switch>
+//   <Route path='/' Component={Home} exact></Route>
+//   <Route path='/department' Component={Department}></Route>
+//   <Route path='/employee' Component={Employee}></Route>
+//  </Switch> */}
+//  <h3 className="m-3 d-flex justity-content-center">React Js with wep api demo.</h3>
+//  <h5 className="m-3 d-flex justity-content-center">Meeting Management portal.</h5>
+//  <Navigation/>
+//  <Routes>
+//         <Route path='/' Component={Home} exact />
+//         <Route path='/department' Component={Department} />
+//         <Route path='/employee' Component={Employee}/>
+//       </Routes>
+//     </div>
+//     </BrowserRouter>
+
+<Navbar/>
+  );
+}
+
+export default App;
---
+++ src/components/aboutus.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function aboutus() {
+  return (
+    <div>aboutus</div>
+  )
+}
+
